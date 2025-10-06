793cc1ac1cf569d4fe4dd423cb6966f2b071632b
Leandra Costa Ramos <159392977+LeandraCR@users.noreply.github.com>
2025-10-06 20:11:58 -0300

    Remove base config para Vercel
    Remove base config para Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+// vite.config.js
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/leandracr-site/", // <-- A linha mais importante de todas
   plugins: [react()],
+  // base: '/leandracr-site/', //
 })
