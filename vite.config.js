0a662257de1fbd93ceb862d21fb71dd6e0ab13af
Leandra Costa Ramos <159392977+LeandraCR@users.noreply.github.com>
2025-10-06 16:52:59 -0300

    Adiciona permissões de escrita ao workflow de deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/leandracr-site/", 
+  base: "/leandracr-site/", // <-- A linha mais importante de todas
   plugins: [react()],
 })
